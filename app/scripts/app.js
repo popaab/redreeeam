868163d98b38c83921a7e29a86ce7655d8cf2c20
popaab <kkl34@sfu.ca>
2015-04-10 20:19:19 -0700

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name redreamApp
+ * @description
+ * # redreamApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('redreamApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngMaterial',
+    'ngSanitize',
+    'ngTouch',
+    'ngGiphy',
+    'ngTagsInput',
+    'ngMaterial'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/redream', {
+        templateUrl: 'views/redream.html',
+        controller: 'RedreamCtrl'
+      })
+      .when('/refeel', {
+        templateUrl: 'views/refeel.html',
+        controller: 'RecallCtrl'
+      })
+
+      .when('/recall', {
+        templateUrl : 'views/recall.html',
+        controller  : 'RecallCtrl'
+      })
+
+      .when('/results', {
+        templateUrl : 'views/results.html',
+        controller  : 'ResultsCtrl'
+      })
+
+      .otherwise({
+        redirectTo: '/redream'
+      });
+  });
